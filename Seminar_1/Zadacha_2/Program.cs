6d85d54f7e834f1527f4179435b9a097a8aba1d3
Maksim Olehnovich <maksm.olehnovich@tut.by>
2022-06-06 23:20:08 +0300

    В задаче 2 семинара 1 добавлена проверка на корректное значение дня, введеное пользователем

--- Seminar_1/Zadacha_2/Program.cs
+++ Seminar_1/Zadacha_2/Program.cs
@@ -1,23 +1,31 @@
 ﻿int day_num;
 
-Console.Write("Введите номер дня недели: ");
+while (true)
+{
+    Console.Write("Введите номер дня недели: ");
 
-day_num = Cosole.Readline();
+    day_num = Convert.ToInt32(Console.ReadLine());
 
-if (day_num < 1 || day_num > 7)
-    Console.WriteLine("Веденно неверное значение");
+    if (day_num < 1 || day_num > 7)
+    {
+        Console.WriteLine($"Веденно неверное значение - {day_num}");
+        continue;
+    }
+
+    break;
+}
 
 if (day_num == 1)
-    Console.WriteLine("Понедельник");
+    Console.WriteLine($"День {day_num} - Понедельник");
 if (day_num == 2)
-    Console.WriteLine("Вторник");
+    Console.WriteLine($"День {day_num} - Вторник");
 if (day_num == 3)
-    Console.WriteLine("Среда");
+    Console.WriteLine($"День {day_num} - Среда");
 if (day_num == 4)
-    Console.WriteLine("Четверг");
+    Console.WriteLine($"День {day_num} - Четверг");
 if (day_num == 5)
-    Console.WriteLine("Пятница");
+    Console.WriteLine($"День {day_num} - Пятница");
 if (day_num == 6)
-    Console.WriteLine("Суббота");
+    Console.WriteLine($"День {day_num} - Суббота");
 if (day_num == 7)
-    Console.WriteLine("Восресенье");
+    Console.WriteLine($"День {day_num} - Восресенье");
