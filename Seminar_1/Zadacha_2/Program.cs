e6048d6425fb866df1ee541e36df187a70228440
Maksim Olehnovich <maksm.olehnovich@tut.by>
2022-06-06 21:16:02 +0300

    seminar_1 folder added

---
+++ Seminar_1/Zadacha_2/Program.cs
@@ -0,0 +1,23 @@
+﻿int day_num;
+
+Console.Write("Введите номер дня недели: ");
+
+day_num = Cosole.Readline();
+
+if (day_num < 1 || day_num > 7)
+    Console.WriteLine("Веденно неверное значение");
+
+if (day_num == 1)
+    Console.WriteLine("Понедельник");
+if (day_num == 2)
+    Console.WriteLine("Вторник");
+if (day_num == 3)
+    Console.WriteLine("Среда");
+if (day_num == 4)
+    Console.WriteLine("Четверг");
+if (day_num == 5)
+    Console.WriteLine("Пятница");
+if (day_num == 6)
+    Console.WriteLine("Суббота");
+if (day_num == 7)
+    Console.WriteLine("Восресенье");
