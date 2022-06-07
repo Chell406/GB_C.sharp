da139437e7c4e11f0980c3e7a0a9268d0642b24c
Maksim Olehnovich <maksm.olehnovich@tut.by>
2022-06-07 12:54:55 +0300

    Добавлена перевод string input в int N методом int.TryParse(input, out N)

--- Seminar_1/Zadacha_4/Program.cs
+++ Seminar_1/Zadacha_4/Program.cs
@@ -1,4 +1,12 @@
-﻿int N = Convert.ToInt32(Console.ReadLine());
+﻿Console.Write("Введите число N: ");
 
+//int N = Convert.ToInt32(Console.ReadLine());
+
+string input = Console.ReadLine();
+int N;
+int.TryParse(input, out N);
+
+Console.WriteLine($"N = {N}");
+Console.WriteLine("Последовательность целых чисел от -N до N:");
 for (int i = -N; i <= N; i++)
     Console.Write($" {i}");
