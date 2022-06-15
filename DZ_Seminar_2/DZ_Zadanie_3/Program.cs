6962c1dcfd0adb236f83ae1840201e2b639154c3
Maksim Olehnovich <maksm.olehnovich@tut.by>
2022-06-15 21:51:04 +0300

    Задание 3 выполнено

---
+++ DZ_Seminar_2/DZ_Zadanie_3/Program.cs
@@ -0,0 +1,20 @@
+﻿int dayNum;
+
+while(true)
+{
+    Console.WriteLine("Введите номер дня недели (от 1 до 7):");
+    dayNum = Convert.ToInt32(Console.ReadLine());
+    if((dayNum >= 1) && (dayNum<=7))
+        break;
+    else
+        Console.WriteLine("Введено неверное значение. Ожидается число от 1 до 7 включительно.");
+}
+
+if (dayNum >= 1 && dayNum <= 5)
+{
+    Console.WriteLine("Будний");
+}
+else 
+{
+    Console.WriteLine("Выходной");
+}
