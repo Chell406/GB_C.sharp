e197a11cf28cdedba6d464196c5027b92db4f869
Maksim Olehnovich <maksm.olehnovich@tut.by>
2022-06-15 21:41:45 +0300

    Задание 2 выполнено

---
+++ DZ_Seminar_2/DZ_Zadanie_2/Program.cs
@@ -0,0 +1,11 @@
+﻿string number;
+while(true){
+    Console.WriteLine("Введите число");
+    number = Console.ReadLine();
+    if(Convert.ToInt32(number) != 0)
+        break;
+}
+if(number.Length >= 3)
+    Console.WriteLine(number[2]);
+else
+    Console.WriteLine("У числа нет третей цифры");
