6c4c0e8e8eca8dc9bd685c6ccfd4b7c183f0b068
Maksim Olehnovich <maksm.olehnovich@tut.by>
2022-06-19 20:32:48 +0300

    Задача 19 выполнена

--- DZ_Seminar_3/DZ_Zadacha_19/Program.cs
+++ DZ_Seminar_3/DZ_Zadacha_19/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool palindrome(int number)
+{
+    int Temp = number;
+    int result = 0;
+    while (true)
+    {
+        result += Temp % 10;
+        Temp = (Temp - (Temp%10)) / 10;
+        if (Temp > 0)
+            result = result * 10;
+        else
+            break;
+    }
+
+    if (number == result)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int number;
+
+// Ввод значения через консоль
+// Console.Write("Введите число: ");
+// number = Convert.ToInt32(Console.ReadLine());
+
+number = 89012;
+
+if(palindrome(number))
+{
+    Console.Write($"Число {number} палиндром");
+}
+else
+{
+    Console.Write($"Число {number} не палиндром");
+}
