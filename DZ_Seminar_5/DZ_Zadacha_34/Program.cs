611c9fbf35ddc212be999a54480ed65257e84b5c
Maksim Olehnovich <maksm.olehnovich@tut.by>
2022-06-21 22:25:41 +0300

    Задача 34 решена

--- DZ_Seminar_5/DZ_Zadacha_34/Program.cs
+++ DZ_Seminar_5/DZ_Zadacha_34/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] newRandomMass(int masSize)
+{
+    int[] mas = new int[masSize];
+
+    for (int i = 0; i < masSize; i++)
+    {
+        mas[i] = new Random().Next(100, 1000);
+    }
+    return mas;
+}
+
+void outMass(int[] mas)
+{
+    int lenght = mas.Length;
+    for (int i = 0; i < lenght; i++)
+    {
+        Console.Write($"{mas[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int positivCount(int[] mas)
+{
+    int countPositiv = 0;
+
+    for (int i = 0; i < mas.Length; i++)
+        if(mas[i] % 2 == 0)
+            countPositiv++;
+    
+    return countPositiv;
+}
+
+
+int[] massiv = newRandomMass(5);
+outMass(massiv);
+Console.WriteLine(positivCount(massiv));
