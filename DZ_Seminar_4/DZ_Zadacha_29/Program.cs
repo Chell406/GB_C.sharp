a9d45ebc7fdee17d265361eb236b8806cbaf27a9
Maksim Olehnovich <maksm.olehnovich@tut.by>
2022-06-21 10:22:07 +0300

    Задача 29 решена

--- DZ_Seminar_4/DZ_Zadacha_29/Program.cs
+++ DZ_Seminar_4/DZ_Zadacha_29/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿int[] newRandomMass(int masSize)
+{
+    int[] mas = new int[masSize];
+
+    for (int i = 0; i < masSize; i++)
+    {
+        mas[i] = new Random().Next();
+    }
+    return mas;
+}
+
+void outMass(int[] mas)
+{
+    int lenght = mas.Length;
+    for (int i = 0; i < lenght; i++)
+    {
+        Console.Write($"{mas[i]} ");
+    }
+}
+
+int size = 8;
+int[] massiv = newRandomMass(size);
+outMass(massiv);
