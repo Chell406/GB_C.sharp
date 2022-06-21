7438fb6079fa67a4d90dfd21b5b2a165ea17e31c
Maksim Olehnovich <maksm.olehnovich@tut.by>
2022-06-21 11:13:21 +0300

    Задача 25 решена. При возведении в отрицательную степень возвращает 0.

--- DZ_Seminar_4/DZ_Zadacha_25/Program.cs
+++ DZ_Seminar_4/DZ_Zadacha_25/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿double myPow(double baseNum, int strenght)
+{
+    if (strenght < 0)
+        return 0;
+    if (strenght == 0)
+        return 1.0;
+    double result = baseNum;
+    for (int i = 0; i <= strenght; i++)
+    {
+        result *= baseNum;
+    }
+    return result;
+}
+
+Console.WriteLine(myPow(456, -6));
+Console.WriteLine(myPow(456, 0));
+Console.WriteLine(myPow(-3, 3));
+Console.WriteLine(myPow(3, 2));
+Console.WriteLine(myPow(2, 5));
+Console.WriteLine(myPow(0.5, 3));
