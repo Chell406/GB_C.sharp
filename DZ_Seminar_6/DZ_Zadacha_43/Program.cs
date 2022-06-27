ba81e522128d9d896f7153d7b9c06f17969895f8
Maksim Olehnovich <maksm.olehnovich@tut.by>
2022-06-27 20:54:28 +0300

    Добавлена пустая папка для задачи 42. Задача 41 решена линейным способом

---
+++ DZ_Seminar_6/DZ_Zadacha_43/Program.cs
@@ -0,0 +1,15 @@
+﻿double  k1, b1, k2, b2;
+double x = 0, y = 0;
+Console.WriteLine("Введите b1:");
+b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите k1:");
+k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите b2:");
+b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите k2:");
+k2 = Convert.ToDouble(Console.ReadLine());
+
+x = (b2 - b1) / (k1 - k2);
+y = (k1 * x) + b1;
+
+Console.WriteLine($"Лини пересекаются в точке ({x},{y})");
