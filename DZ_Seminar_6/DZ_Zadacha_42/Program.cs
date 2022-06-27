f074b5b1846eda585008abafdab2097082fd238b
Maksim Olehnovich <maksm.olehnovich@tut.by>
2022-06-27 21:44:18 +0300

    Задача 42 решена

--- DZ_Seminar_6/DZ_Zadacha_42/Program.cs
+++ DZ_Seminar_6/DZ_Zadacha_42/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿int _10_to(int input, int osnovanie) {
+
+	int ostatok;
+	ostatok = input % osnovanie;
+	int resultat;
+	resultat = (input - ostatok) / osnovanie;
+	if (resultat < osnovanie)
+	{
+		return (resultat * 10) + ostatok;
+	}
+	else
+	{
+		return (_10_to(resultat, osnovanie) * 10) + ostatok;
+	}
+}
+
+Console.WriteLine("Введите число:");
+int numberFromInput = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите основание:");
+int osnovanie = Convert.ToInt32(Console.ReadLine());
+
+int numberForOutput = _10_to(numberFromInput, osnovanie);
+Console.WriteLine($"Число в {osnovanie} системе: {numberForOutput}");
