fb3d6b5a8dafb66dee488aa9738f263b609c519f
Maksim Olehnovich <maksm.olehnovich@tut.by>
2022-06-27 20:28:00 +0300

    Добавлена папка с ДЗ семинара 6. Задача 41 решена

---
+++ DZ_Seminar_6/DZ_Zadacha_41/Program.cs
@@ -0,0 +1,27 @@
+﻿int funcion()
+{
+    Console.WriteLine("Введите число:");
+    int number = Convert.ToInt32(Console.ReadLine());
+    int countOfZero = 0;
+    int _continue = 0;
+
+    if (number == 0)
+        countOfZero++;
+
+    while (true)
+    {
+        Console.WriteLine("Продолжить ввод?(1-да,0-нет)");
+        _continue = Convert.ToInt32(Console.ReadLine());
+        if (_continue == 0 || _continue == 1)
+            break;
+    }
+    if (_continue == 1)
+        return countOfZero + funcion();
+    else
+    {
+        return countOfZero;
+    }
+
+}
+
+Console.WriteLine($"Вы ввели число '0' {funcion()} раз");
