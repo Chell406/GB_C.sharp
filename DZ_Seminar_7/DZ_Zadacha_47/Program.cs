fc83e06314d274090bb6a2019676b0d292022766
Maksim Olehnovich <maksm.olehnovich@tut.by>
2022-07-02 19:53:54 +0300

    Задача 47 решена

--- DZ_Seminar_7/DZ_Zadacha_47/Program.cs
+++ DZ_Seminar_7/DZ_Zadacha_47/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void RandomFilling(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Math.Round((((new Random().NextDouble()) * 20) - 10), 2);
+        }
+    }
+}
+
+void ShowArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int row = 4, column = 6;
+
+double[,] array = new double[row, column];
+
+RandomFilling(array);
+ShowArray(array);
+
