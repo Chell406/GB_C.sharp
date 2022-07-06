ec7096dbf50d0404ba940787256dfae1209f91cb
Maksim Olehnovich <maksm.olehnovich@tut.by>
2022-07-07 02:22:37 +0300

    Задача 56 решена

--- DZ_Seminar_8/DZ_Zadacha_56/Program.cs
+++ DZ_Seminar_8/DZ_Zadacha_56/Program.cs
@@ -1,2 +1,156 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+int NumOfMinimalSumRow(int[,] array)
+{
+    int rowWithMinimalSum = -1;
+    int minSumOfElements = 0;
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int sumOfElements = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sumOfElements += array[i, j];
+        }
+
+        if (sumOfElements < minSumOfElements)
+        {
+            minSumOfElements = sumOfElements;
+            rowWithMinimalSum = i + 1;
+        }
+    }
+    return rowWithMinimalSum;
+}
+
+
+
+
+int[,] array = new int[5, 6];
+MyArray.RandomFilling(array, 10);
+Console.WriteLine("Исходный массив:");
+MyArray.Show(array);
+
+int rowWithMinimalSumOfElem = NumOfMinimalSumRow(array);
+Console.WriteLine($"Строка с наименьшей суммой элементов: {rowWithMinimalSumOfElem}");
+
+class MyArray
+{
+    public static void Show(int[,] array)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write($"{array[i, j]}  ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public static void Show(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write($"  {array[i]}");
+        }
+        Console.WriteLine();
+    }
+
+    // Заполнение массива рандомными целыми значениями от -max до max 
+    public static void RandomFilling(int[,] array, int max)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array[i, j] = (new Random().Next(max) * 2) - max;
+            }
+        }
+    }
+
+    //Среднее арифметическое всех столбцов с округлением до 4 знаков
+    public static double[] AverageOfColumns(int[,] array)
+    {
+        int rows = array.GetLength(0);
+        int columns = array.GetLength(1);
+
+        double[] averageValue = new double[columns];
+
+        for (int i = 0; i < columns; i++)
+        {
+            double sumOfElementsInRows = 0;
+            for (int j = 0; j < rows; j++)
+            {
+                sumOfElementsInRows += array[j, i];
+            }
+            averageValue[i] = Math.Round((sumOfElementsInRows / rows), 4);
+        }
+
+        return averageValue;
+    }
+
+    int SumOfElements(int[] array)
+    {
+        int sumOfElements = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            sumOfElements += array[i];
+        }
+        return sumOfElements;
+    }
+
+    public static void Show(double[,] array)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write($"{array[i, j]}  ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public static void Show(double[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write($"  {array[i]}");
+        }
+        Console.WriteLine();
+    }
+
+    //Заполнение массива дробными числами от -max до max
+    //с округлением до 2 знаков после запятой
+    public static void RandomFilling(double[,] array, int max)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array[i, j] = Math.Round((((new Random().NextDouble()) * 2 * max) - max), 2);
+            }
+        }
+    }
+
+    //Среднее арифметическое всех столбцов с округлением до 4 знаков
+    public static double[] AverageOfColumns(double[,] array)
+    {
+        int rows = array.GetLength(0);
+        int columns = array.GetLength(1);
+
+        double[] averageValue = new double[columns];
+
+        for (int i = 0; i < columns; i++)
+        {
+            double sumOfElementsInRows = 0;
+            for (int j = 0; j < rows; j++)
+            {
+                sumOfElementsInRows += array[j, i];
+            }
+            averageValue[i] = Math.Round((sumOfElementsInRows / rows), 4);
+        }
+
+        return averageValue;
+    }
+}
