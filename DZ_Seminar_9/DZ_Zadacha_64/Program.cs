de9f7a9fa8d2a1f47463cdaa555b67a7f7a6b9a0
Maksim Olehnovich <maksm.olehnovich@tut.by>
2022-07-07 04:23:11 +0300

    Задача 64 решена

--- DZ_Seminar_9/DZ_Zadacha_64/Program.cs
+++ DZ_Seminar_9/DZ_Zadacha_64/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿void Natural (int lowLim, int highLim)
+{
+    if(lowLim!=highLim)
+    {
+        Console.Write($"{lowLim} ");
+        Natural(lowLim + 1, highLim);
+    }
+}
+
+int M = 10, N = 23;
+Console.WriteLine($"Последовательность натуральных чисел от {M} до {N}: ");
+Natural(M, N);
